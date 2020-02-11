f19ad5cf58410500dc53d8343bb73968200de87b
Codee Palmer <codeejpalmer@gmail.com>
2020-02-10 17:54:32 -0800

    Updated About section text

--- src/components/about.js
+++ src/components/about.js
@@ -7,26 +7,12 @@ export default function About() {
     <div className="about">
       <h1 className="about__heading">About Me</h1>
       <Welcome />
-      <p className="about__text">
-        I’m a recent Web Development student just graduating from BrainStation
-        Vancouver. Before my journey in web development I was and Officer on
-        Private yachts.
-      </p>{" "}
-      <p className="about__text">
-        After years of working as an Officer on Yachts I had a yearning for
-        change. Taking courses on Udemy I started to teach myself Web
-        Development. I was hooked! I started working on personal projects and
-        decided to take my skills and knowledge a step forward do my formal
-        studies to further progress my personal and professional goals.
-      </p>{" "}
-      <p className="about__text">
-        {" "}
-        Having completed at BrainStation (Vancouver) just over a month ago I
-        have taken up some new freelance work and continued to progress my
-        skills wherever I can. I have just moved down to San Diego, CA and I can
-        not wait to grow and progress my skills in Development both personally
-        and professionally.
-      </p>
+      <h4 className="about__text">
+        Innovative optimized solution seeker | Full-Stack Web Developer |
+        Ambitious and fast learning, committed to achieving high levels of
+        success, meeting tight deadlines, and consistently delivering superior
+        performance |
+      </h4>
       <Arrow />
     </div>
   );
