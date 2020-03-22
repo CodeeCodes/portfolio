a6b6817056d4113b95f08960138a3e2658ae750d
Codee J Palmer <codeecodes92@gmail.com>
2020-03-22 13:20:59 -0700

    Fixed prevProjects section for now by rolling back, going to test js carousel again

--- src/components/mainContent.js
+++ src/components/mainContent.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./header";
 import Hero from "./hero";
 import Work from "./prevWork";
-import Projects from "./prevProjects2";
+import Projects from "./prevProjects";
 export default function MainContent() {
   return (
     <div className="main">
