059d2d400ed3b88124778a8cf12bf560f19a0960
Codee J Palmer <codeecodes92@gmail.com>
2020-03-23 13:20:16 -0700

    Changed background for about section, added  background to footer and restyled prevWork section

--- src/components/about.js
+++ src/components/about.js
@@ -5,14 +5,16 @@ import Arrow from "./downArrow";
 export default function About() {
   return (
     <div className="about">
-      <h1 className="about__heading">About Me</h1>
-      <Welcome />
-      <h4 className="about__text">
-        | Full-Stack Web Developer | Innovative optimized solution seeker |
-        Ambitious and fast learning, committed to achieving high levels of
-        success, meeting tight deadlines, and consistently delivering superior
-        performance |
-      </h4>
+      <div className="about__div">
+        {/* <h1 className="about__heading">About Me</h1> */}
+        <Welcome />
+        <h4 className="about__text">
+          | Full-Stack Web Developer | Innovative optimized solution seeker |
+          Ambitious and fast learning, committed to achieving high levels of
+          success, meeting tight deadlines, and consistently delivering superior
+          performance |
+        </h4>
+      </div>
       <Arrow />
     </div>
   );
